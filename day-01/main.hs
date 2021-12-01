2b73625cd58e7acbc4eeb872453e2aaf1a1f8657
Bharath Chandra Sudheer <bharath.sudheer@gmail.com>
2021-12-02 00:31:23 +0800

    Solve part 2 (sliding window)

--- day-01/main.hs
+++ day-01/main.hs
@@ -1,20 +1,31 @@
-partOne :: [Integer] -> Integer
-partOne (x:xs) = x
-partOne [] = 0
+main :: IO()
+main = do
+  fileContents <- readFile "input1.txt"
+  print "Part One:"
+  print $ analysePartOne fileContents
+  print "Part Two:"
+  print $ analysePartTwo fileContents
 
-analyse :: String -> Integer
-analyse = countIncreasing . parse
+-- core functions
+
+analysePartOne :: String -> Integer
+analysePartOne = countIncreasing . parse
+
+analysePartTwo :: String -> Integer
+analysePartTwo = countIncreasing . combineThree . parse
+
+-- helper functions
 
 parse :: String -> [Integer]
 parse input = map (read :: String -> Integer) (lines input)
 
 countIncreasing :: [Integer] -> Integer 
-countIncreasing (x:y:xss) = (if y > x then 1 else 0) + countIncreasing(y:xss)
+countIncreasing (x:y:xs) = (if y > x then 1 else 0) + countIncreasing(y:xs)
 countIncreasing [x] = 0
 countIncreasing [] = 0
 
-main :: IO()
-main = do
-  fileContents <- readFile "input1.txt"
-  print "Part One:"
-  print $ analyse fileContents
+combineThree :: [Integer] -> [Integer]
+combineThree (x:y:z:xs) = x+y+z: combineThree (y:z:xs)
+combineThree [x, y] = []
+combineThree [x] = []
+combineThree [] = []
