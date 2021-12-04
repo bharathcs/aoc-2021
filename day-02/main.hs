56178cf4b3ff53652fbdb82bf441ea2dbe6e31b1
Bharath Chandra Sudheer <bharathcs@users.noreply.github.com>
2021-12-04 12:18:06 +0800

    Solve day 2 part 2.

--- day-02/main.hs
+++ day-02/main.hs
@@ -4,11 +4,19 @@ main :: IO()
 
 main = do
   fileContents <- readFile "input.txt"
+
   print "Part One:"
   let res = analysePartOne fileContents
-  let output = "h: " ++ show (h res) ++ " v: " ++ show (v res)
-  print output
+  print $ "h: " ++ show (h res) ++ " v: " ++ show (v res)
   print $ "product: " ++ show (h res * v res)
+
+  print "Part Two:"
+  let res2 = analysePartTwo fileContents
+  print $ posToString res2
+  print $ "product " ++ show (dist res2 * d res2)
+
+
+-- Part One
 
 data IntVector = IntVector {h :: Int, v :: Int}
 
@@ -35,3 +43,24 @@ addVectors a b = IntVector {h = h a + h b, v = v a + v b}
 
 sumVectors :: [IntVector] -> IntVector
 sumVectors = foldl addVectors IntVector {h = 0, v = 0}
+
+-- Part Two
+
+analysePartTwo :: String -> TruePosition
+analysePartTwo input = calculate (map lineToTruePosition (lines input))
+
+lineToTruePosition :: String -> TruePosition
+lineToTruePosition = (\p -> TruePosition {d = 0, dist = h p, a = v p}) . lineToVector
+
+calculate :: [TruePosition] -> TruePosition
+getNextPosition :: TruePosition -> TruePosition -> TruePosition
+
+data TruePosition = TruePosition {d :: Int, dist :: Int, a :: Int}
+posToString :: TruePosition -> [Char]
+posToString p = "depth: " ++ show (d p) ++ " dist: " ++ show (dist p) ++ " aim: " ++ show (a p)
+
+calculate = foldl getNextPosition zeroPosition
+  where zeroPosition = TruePosition {d = 0, dist = 0, a = 0}
+
+getNextPosition p1 p2  = TruePosition { d = newDepth, dist = dist p1 + dist p2, a = a p1 + a p2 }
+  where newDepth = d p1 + dist p2 * (a p1 + a p2)
