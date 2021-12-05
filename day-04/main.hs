1474149bce6707362ee6cb5dbadb27622d35810c
Bharath Chandra Sudheer <bharathcs@users.noreply.github.com>
2021-12-05 22:29:13 +0800

    Refactor for cleanliness.

--- day-04/main.hs
+++ day-04/main.hs
@@ -1,16 +1,13 @@
 main :: IO ()
 
 main = do
-
-  let sample = "7,4,9,5,11,17,23,2,0,14,21,24,10,16,13,6,15,25,12,22,18,20,8,19,3,26,1\n22 13 17 11  0\n 8  2 23  4 24\n21  9 14 16  7\n 6 10  3 18  5\n 1 12 20 15 19\n 3 15  0  2 22\n 9 18 13 17  5\n19  8  7 25 23\n20 11 10 24  4\n14 21 16 12  6\n14 21 17 24  4\n10 16 15  9 19\n18  8 23 26 20\n22 11 13  6  5\n 2  0 12  3  7\n"
+  -- let sample = "7,4,9,5,11,17,23,2,0,14,21,24,10,16,13,6,15,25,12,22,18,20,8,19,3,26,1\n22 13 17 11  0\n 8  2 23  4 24\n21  9 14 16  7\n 6 10  3 18  5\n 1 12 20 15 19\n 3 15  0  2 22\n 9 18 13 17  5\n19  8  7 25 23\n20 11 10 24  4\n14 21 16 12  6\n14 21 17 24  4\n10 16 15  9 19\n18  8 23 26 20\n22 11 13  6  5\n 2  0 12  3  7\n"
 
   fileContents <- readFile "input.txt"
-  print $ length $ lines fileContents
-  let (x, y) = parse fileContents
-
-  print $ length y
-  print $ showBoard $ y !! 98
-  print $ getRightBoard y (0:x)
+  let (drawnNums, boards) = parse fileContents
+  
+  print "Part One: "
+  print $ getRightBoard boards (0:drawnNums)
 
 -- Utils
 
@@ -61,3 +58,4 @@ getRightBoard boards nums = if not (null winningBoard)
     currNum = head nums
     nextNum = head $ tail nums
 
+-- Part 2
