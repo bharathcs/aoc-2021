0006571ffd4895d0b913486bd0987f8e8f4546fc
Bharath Chandra Sudheer <bharathcs@users.noreply.github.com>
2021-12-06 17:51:57 +0800

    Solve Day 6 Part 2

--- day-06/main.hs
+++ day-06/main.hs
@@ -4,8 +4,10 @@ main = do
     fileContents <- readFile "input.txt"
     let population = parse fileContents
 
-    let simulate80Days = repeater 80 simulate
-    print $ length $ simulate80Days population
+    let simulate80Days = "After " ++ show 80 ++ " days: " ++ show (length (repeater 80 simulate population)) -- Part 1
+    let simulate256Days = "After " ++ show 256 ++ " days: " ++ show (sumPop (repeater 256 optimisedSimulate (getPop population))) -- Part 2
+    print simulate80Days
+    print simulate256Days
 
 -- utils
 type LanternAge = Int
@@ -33,4 +35,20 @@ simulate (x:xs)
     | x == 0 = 6 : 8 : simulate xs
     | otherwise = x - 1 : simulate xs
 simulate [] = []
+
+-- Part Two
+
+type Pop = (Int, Int, Int, Int, Int, Int, Int, Int, Int) -- day 0 to 8
+
+getPop :: [Int] -> Pop
+getPop arr = let [a, b, c, d, e, f, g, h, i] = countIntoBuckets 9 arr in (a, b, c, d, e, f, g, h, i)
+
+countIntoBuckets :: Int -> [Int] -> [Int]
+countIntoBuckets n xs = map (length . (\x -> filter (==x) xs)) [0..(n-1)]
+
+sumPop :: Pop -> Int 
+sumPop (a, b, c, d, e, f, g, h, i) = a + b + c + d + e + f + g + h + i
+
+optimisedSimulate :: Pop -> Pop
+optimisedSimulate (a, b, c, d, e, f, g, h, i) = (b, c, d, e, f, g, h + a, i, a)
 
